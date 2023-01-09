1c97169267ee98032a56c1f5ea36c5430097fea7
FedeLopez17 <fedelopez1712@gmail.com>
2023-01-09 18:44:19 -0300

    Finish 7th task

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 function sumRange(num) {
-  //Base case
+  // Base case
   if (num === 1) return 1;
 
-  //Recursive case
+  // Recursive case
   return num + sumRange(--num);
 }
 
 function power(base, exponent) {
-  //Base case
+  // Base case
   if (exponent === 0) return 1;
 
-  //Recursive case
+  // Recursive case
   return base * power(base, --exponent);
 }
 
 function factorial(number) {
-  //Base case
+  // Base case
   if (number === 1) return 1;
 
-  //Recursive case
+  // Recursive case
   return number * factorial(--number);
 }
 
 function all(arr, callback) {
-  //Base case
+  // Base case
   if (arr.length === 1) return callback(arr[0]);
 
-  //Recursive case
+  // Recursive case
   const firstElement = arr.shift();
   return callback(firstElement) && all(arr, callback);
 }
 
 function productOfArray(arr) {
-  //Base case
+  // Base case
   if (arr.length === 1) return arr[0];
 
-  //Recursive case
+  // Recursive case
   const firstElement = arr.shift();
   return firstElement * productOfArray(arr);
 }
@@ -47,15 +47,49 @@ function productOfArray(arr) {
 // According to the solution provided, a "wrapper" loop could be used for this, which makes a lot of sense.
 function contains(targetValue, obj) {
   for (const key in obj) {
-    //Base case
+    // Base case
     if (obj[key] === targetValue) {
       return true;
     }
 
-    //Recursive case
+    // Recursive case
     if (typeof obj[key] === "object") {
       return contains(targetValue, obj[key]);
     }
   }
   return false;
+}
+
+// I struggled with this task as well, but I was able to solve it by myself.
+// I used a loop whereas the solution provided did not.
+
+// MY SOLUTION:
+function totalIntegers(arr) {
+  // Base case
+  if (!Array.isArray(arr)) {
+    return typeof arr === "number" && Number.isInteger(arr) ? 1 : 0;
+  }
+
+  // Recursive case
+  let total = 0;
+  for (const item of arr) {
+    total += totalIntegers(item);
+  }
+  return total;
+}
+
+// SOLUTION PROVIDED:
+function totalIntegersSolution(array) {
+  if (array.length === 0) return 0;
+
+  let total = 0;
+  let first = array.shift();
+
+  if (Array.isArray(first)) {
+    total += totalIntegersSolution(first);
+  } else if (Number.isInteger(first)) {
+    total += 1;
+  }
+
+  return total + totalIntegersSolution(array);
 }
