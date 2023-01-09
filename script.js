55a77bdd3d8f431fe09dd42328faa5aa464344eb
FedeLopez17 <fedelopez1712@gmail.com>
2023-01-09 15:39:49 -0300

    Finish 6th task

--- script.js
+++ script.js
@@ -39,3 +39,23 @@ function productOfArray(arr) {
   const firstElement = arr.shift();
   return firstElement * productOfArray(arr);
 }
+
+// I struggled so much with this function, that I decided to look at the solution.
+// I made sure to completely understand what is going on.
+// In my attempts, I was close to the correct solution,
+// but I couldn't figure out how to make the recursive function "branch out" when an object had different properties that where object themselves.
+// According to the solution provided, a "wrapper" loop could be used for this, which makes a lot of sense.
+function contains(targetValue, obj) {
+  for (const key in obj) {
+    //Base case
+    if (obj[key] === targetValue) {
+      return true;
+    }
+
+    //Recursive case
+    if (typeof obj[key] === "object") {
+      return contains(targetValue, obj[key]);
+    }
+  }
+  return false;
+}
